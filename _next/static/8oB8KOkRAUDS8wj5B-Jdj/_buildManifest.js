369c093c439c178b37750420f07a988f27af4114
Wesley Augusto Isotton <wesleya.isotton@gmail.com>
2022-02-26 14:09:16 -0300

    Updates

---
+++ _next/static/8oB8KOkRAUDS8wj5B-Jdj/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/ebc642e66a7b804d.css","static/chunks/pages/index-d0bd2aff363b0311.js"],"/_error":["static/chunks/pages/_error-ace960bde90fa08e.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
